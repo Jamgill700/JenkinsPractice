a3b21fcd30200039539c4a0bb6bb11a729f37e12
James Gillen <jamgill700@gmail.com>
2023-02-11 20:58:33 +0000

    add powershell script

--- test/test.groovy
+++ test/test.groovy
@@ -1,29 +1,32 @@
- #!/usr/bin/env groovy
+#!/usr/bin/env groovy
 
 pipeline {
 
-      parameters{
-      string(name:'param1',  defaultValue:''  ?: '')
-      string(name:'param2',  defaultValue:''  ?: 'test')
-      string(name:'param3',  defaultValue:''  ?: '')
-  }
+//       parameters{
+//       string(name:'param1',  defaultValue:''  ?: '')
+//       string(name:'param2',  defaultValue:''  ?: 'test')
+//       string(name:'param3',  defaultValue:''  ?: '')
+//   }
 
     agent any
-    stage('Build') {
-        steps {
-            sh 'echo ${param1}'
+    stages {
+        stage('Version') {
+            steps {
+                sh 'pwsh --version'
+            }
         }
+
+        stage('hello') {
+            steps {
+                sh 'pwsh testps1'
+            }
+        }
+
+        // stage('Deploy') {
+        //     steps {
+        //         sh 'echo ${param3}'
+        //     }
+        // }
     }
 
-    stage('Test') {
-        steps {
-            sh 'echo ${param2}'
-        }
-    }
-
-        stage('Deploy') {
-        steps {
-            sh 'echo ${param3}'
-        }
-    }
 }
